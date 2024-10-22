6068a330eb2bad5a140831045e02290d560221da
liewchooichin <liewchooichin@gmail.com>
2024-10-22 11:17:42 +0800

    Comment out the default css to let Bootstrap stylesheet take over

--- bus-schedule-tracking-app/src/App.jsx
+++ bus-schedule-tracking-app/src/App.jsx
@@ -1,5 +1,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+// Comment out the App.css to let Bootstrap stylesheets
+// take over.
 //import './App.css'
 //import { BusMain } from './components/BusMain';
 import { RouterProvider } from 'react-router-dom';
--- bus-schedule-tracking-app/src/main.jsx
+++ bus-schedule-tracking-app/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// Comment out the App.css to let Bootstrap stylesheets
+// take over.
 //import './index.css'
 
 createRoot(document.getElementById('root')).render(
